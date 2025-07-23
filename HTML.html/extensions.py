7769f0fb922b508b224aba3863b8a343396f29f3
emiliokamau <151363430+emiliokamau@users.noreply.github.com>
2025-07-24 00:00:12 +0300

    Delete HTML.html directory

--- HTML.html/extensions.py
+++
@@ -1,9 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from flask_login import LoginManager
-#import app
-
-db = SQLAlchemy()
-migrate = Migrate()
-login_manager = LoginManager()
-login_manager.login_view = 'login.html'
